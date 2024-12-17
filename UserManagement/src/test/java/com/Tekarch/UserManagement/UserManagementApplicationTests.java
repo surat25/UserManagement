a862b5c236f28740e01b44555a5b90eab183beed
Surat C <suratc@tekarch.com>
2024-12-17 16:51:14 -0500

    user management

---
+++ UserManagement/src/test/java/com/Tekarch/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tekarch.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
